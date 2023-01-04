5a70be096aaa382c2a2d3c9ca4db3a928fdf832f
kibuelll <kibuellyoi34@gmail.com>
2023-01-04 08:41:49 +0700

    fix conflict

--- services/app/controllers/controllerArt.js
+++ services/app/controllers/controllerArt.js
@@ -1,102 +1,118 @@
-const { Art, Preview, sequelize } = require("../models")
+const { Art, Preview, sequelize } = require("../models");
 
 class ControllerArt {
-    static async createArt(req, res, next) {
-        const t = await sequelize.transaction()
-        try {
+  static async createArt(req, res, next) {
+    const t = await sequelize.transaction();
+    try {
+      let { name, price, description, CategoryId } = req.body;
 
-            let { name, price, description, CategoryId } = req.body
+      ////REQ.FILES RECIEVES AN ARRAY OF 4 OBJECTS. FIRST IS SOURCE FOR ART, THE REST IS FOR PREVIEWS
+      let art = await Art.create({
+        name,
+        price,
+        description,
+        AuthorId: 1,
+        source: req.files[0].path,
+        CategoryId,
+        status: "Active",
+      });
+      let previews = req.files.slice(1);
 
-            ////REQ.FILES RECIEVES AN ARRAY OF 4 OBJECTS. FIRST IS SOURCE FOR ART, THE REST IS FOR PREVIEWS
-            let art = await Art.create({ name, price, description, AuthorId: 1, source: req.files[0].path, CategoryId, status: 'Active' })
-            let previews = req.files.slice(1)
+      let convertedPreviews = previews.map((el) => {
+        el.sourceUrl = el.path;
+        el.ArtId = art.dataValues.id;
+        return el;
+      });
 
-            let convertedPreviews = previews.map(el => {
-                el.sourceUrl = el.path
-                el.ArtId = art.dataValues.id
-                return el
-            })
+      let newPreviews = await Preview.bulkCreate(convertedPreviews);
 
-            let newPreviews = await Preview.bulkCreate(convertedPreviews)
-
-            t.commit()
-            art.dataValues.Previews = newPreviews
-            res.status(201).json({ art })
-        } catch (error) {
-            next(error)
-        }
+      t.commit();
+      art.dataValues.Previews = newPreviews;
+      res.status(201).json({ art });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async getArts(req, res, next) {
-        try {
-
-            let arts = await Art.findAll({ include: [{ model: Preview }] })
-            // console.log(arts);
-            res.status(200).json(arts)
-        } catch (error) {
-            console.log(error);
-            // next(error)
-        }
+  static async getArts(req, res, next) {
+    try {
+      let arts = await Art.findAll({ include: [{ model: Preview }] });
+      // console.log(arts);
+      res.status(200).json(arts);
+    } catch (error) {
+      console.log(error);
+      // next(error)
     }
+  }
 
-    static async getOneArt(req, res, next) {
-        try {
+  static async getOneArt(req, res, next) {
+    try {
+      let { id } = req.params;
+      let art = await Art.findOne({
+        where: { id },
+        include: [{ model: Preview }],
+      });
 
-            let { id } = req.params
-            let art = await Art.findOne({ where: { id }, include: [{ model: Preview }] })
+      if (!art) {
+        throw { name: "NOT FOUND" };
+      }
 
-            if (!art) {
-                throw { name: "NOT FOUND" }
-            }
-
-            res.status(200).json(art)
-        } catch (error) {
-            next(error)
-        }
+      res.status(200).json(art);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async deleteArt(req, res, next) {
-        try {
-            const { id } = req.params
-            // note: status default pas create: active. Kalo mau seeding jangan lupa tambahin di seeders
-            await Art.update({ status: 'inactive' }, { where: { id } })
-
-        } catch (error) {
-            next(error)
-        }
+  static async deleteArt(req, res, next) {
+    try {
+      const { id } = req.params;
+      // note: status default pas create: active. Kalo mau seeding jangan lupa tambahin di seeders
+      await Art.update({ status: "inactive" }, { where: { id } });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async updateArt(req, res, next) {
-        const t = await sequelize.transaction()
+  static async updateArt(req, res, next) {
+    const t = await sequelize.transaction();
 
-        try {
-            const { id } = req.params
-            const { name, price, description, CategoryId, status } = req.body
+    try {
+      const { id } = req.params;
+      const { name, price, description, CategoryId, status } = req.body;
 
-            // tolong dicek lagi yg source/req files udah benar apa belum
-            // nanti AuthorId pake req user, skrg masih 1
+      // tolong dicek lagi yg source/req files udah benar apa belum
+      // nanti AuthorId pake req user, skrg masih 1
 
-            const updatedArt = await Art.update({ name, price, description, AuthorId: 1, source: req.files[0].path, CategoryId, status }, { where: { id } })
+      const updatedArt = await Art.update(
+        {
+          name,
+          price,
+          description,
+          AuthorId: 1,
+          source: req.files[0].path,
+          CategoryId,
+          status,
+        },
+        { where: { id } }
+      );
 
-            const previews = req.files.slice(1)
+      const previews = req.files.slice(1);
 
-            const convertedPreviews = previews.map(el => {
-                el.sourceUrl = el.path
-                el.ArtId = updatedArt.dataValues.id
-                return el
-            })
+      const convertedPreviews = previews.map((el) => {
+        el.sourceUrl = el.path;
+        el.ArtId = updatedArt.dataValues.id;
+        return el;
+      });
 
-            const newPreviews = await Preview.bulkCreate(convertedPreviews)
+      const newPreviews = await Preview.bulkCreate(convertedPreviews);
 
-            t.commit()
-            updatedArt.dataValues.Previews = newPreviews
-            res.status(200).json({ updatedArt })
-
-        } catch (error) {
-            next(error)
-        }
+      t.commit();
+      updatedArt.dataValues.Previews = newPreviews;
+      res.status(200).json({ updatedArt });
+    } catch (error) {
+      next(error);
     }
-
+  }
 }
 
-module.exports = ControllerArt
+module.exports = ControllerArt;
--- services/app/routes/routeArt.js
+++ services/app/routes/routeArt.js
@@ -9,6 +9,7 @@ const { authorization, checkArtStatus } = require('../middlewares/authorization'
 router.post("/", fileUpload.array("uploadedFile", 4), ControllerArt.createArt)
 router.get("/", ControllerArt.getArts)
 router.get("/:id", ControllerArt.getOneArt)
+// router.get("/download/:id",ControllerArt.download)
 router.delete("/:id", authorization, checkArtStatus, ControllerArt.deleteArt)
 router.put("/:id", authorization, ControllerArt.updateArt)
 
