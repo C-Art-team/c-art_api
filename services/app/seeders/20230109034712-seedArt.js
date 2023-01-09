5c555d952ac394840d2d9e3c723146037d951453
unknown <ian_buzz@hotmail.com>
2023-01-09 14:45:30 +0700

    seedArt&PreviewDone

---
+++ services/app/seeders/20230109034712-seedArt.js
@@ -0,0 +1,27 @@
+'use strict';
+const arts = require("../data/art.json")
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Arts', arts, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Arts', null, {});
+  }
+};
