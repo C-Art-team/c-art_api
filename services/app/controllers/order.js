34f5ae897397a18cb03ad4d46522fef51c36a3df
kibuelll <kibuellyoi34@gmail.com>
2023-01-11 10:59:58 +0700

    refactor amount on controller order

--- services/app/controllers/order.js
+++ services/app/controllers/order.js
@@ -10,7 +10,7 @@ class ControllerOrder {
 
             const artOrdered = await Art.findByPk(artId)
             
-            if (!customerId || amount <= 0 || !artOrdered || artOrdered.AuthorId === +customerId) throw { name: 'INVALID ORDER' }
+            if (!customerId || !artOrdered || artOrdered.AuthorId === +customerId) throw { name: 'INVALID ORDER' }
 
             const newOrder = await Order.create({
                 customerId, artId, amount : 1, status: 'Unpaid', orderDate: new Date()
@@ -19,6 +19,7 @@ class ControllerOrder {
             res.status(201).json(newOrder);
 
         } catch (error) {
+            console.log(error)
             next(error)
         }
 
